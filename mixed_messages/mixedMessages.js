092bf1707931dea261b64856f3e548e59894d28e
Angel Petrov <a.demerdzhiev@gmail.com>
2022-01-28 06:05:20 +0200

    mixed messages portfolio project ready for feedback

--- mixed_messages/mixedMessages.js
+++ mixed_messages/mixedMessages.js
@@ -1,5 +1,25 @@
-// Nonsesnsical group name generator
+// Nonsesnsical book name generator
 
-console.log('Nonsesnsical group name generator')
+console.log('Nonsesnsical Book Title Generator')
 
-let personal_jokes = [];
+let firstWords = ['Quarks', 'Alice', 'Sophia', 'Angel', 'Brother', 'Dragon', 'Lady'
+, 'Girl', 'Dog', 'Cat', 'Seneca', 'Marcus Aurelius'];
+
+let connectingWords = ['and', 'to', 'for', 'the', 'in', 'but', 'is'];
+
+let endWords = ['Heaven', 'Hell', 'Wonderland', 'Ancient World', 'Holly Grail',
+'Temple'];
+
+const randomNumberGenerator = (num) => {return Math.floor(Math.random()*num)};
+
+const generateTitle = () => {
+  let beggining = firstWords[randomNumberGenerator(firstWords.length)];
+  let connectingWord = connectingWords[randomNumberGenerator(
+    connectingWords.length)];
+  let endWord = endWords[randomNumberGenerator(endWords.length)];
+
+  return beggining + ' ' + connectingWord + ' ' + endWord;
+  }
+
+const title = generateTitle();
+console.log(title);
